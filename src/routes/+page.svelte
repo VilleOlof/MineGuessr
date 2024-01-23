9236baa20e2cd66bb923cb31709be0011930cb70
VilleOlof <villeolof@gmail.com>
2024-01-23 03:39:21 +0100

    Bumped version number

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -116,7 +116,7 @@
 		/></svg
 	>
 	<!-- svelte-ignore a11y-click-events-have-key-events -->
-	<p><span on:click={increment_login}>V</span>1.2.0</p>
+	<p><span on:click={increment_login}>V</span>1.2.1</p>
 </div>
 
 {#if show_info}
